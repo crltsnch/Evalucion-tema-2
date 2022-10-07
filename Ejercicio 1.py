622034177052260a5fdb41410287b6effb730cec
crltsnch <crltsnch@myuax.com>
2022-10-07 16:01:48 +0200

    Ejercicio 1 acabado

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,18 +1,23 @@
 class Alumno:
-    def __init__(self, nombre, nota):
+    def iniciar(self, nombre, nota):
         self.nombre = nombre
         self.nota = nota
     def imprimir(self):
         print("Nombre: ", self.nombre)
         print("Nota: ", self.nota)
-    def nota(self):
+    def calificacion(self):
         if self.nota>=5:
             print("Ha aprobado")
         else:
             print("Ha suspendido")
 
-Nombre = input("Escribe el nombre del alumno: ")
-Nota = input("Escriba la nota del alumno: ")
 
-Alumno1 = Alumno('nombre', 'nota')
+Alumno1 = Alumno()
+Alumno1.iniciar("Carla", 3)
 Alumno1.imprimir()
+Alumno1.calificacion()
+
+Alumno2 = Alumno()
+Alumno2.iniciar("Ivan", 7)
+Alumno2.imprimir()
+Alumno2.calificacion()
